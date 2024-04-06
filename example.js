3b30c81c5be2b53e9bca01b56371b779c425b70a
rostyslav.bornitskyi <doofixxx@gmail.com>
2024-04-07 00:09:07 +0300

    use own package

--- example.js
+++ example.js
@@ -1,10 +1,4 @@
-const graphqlFlow = (...fns) => async (...args) => {
-  if (fns.length === 1) {
-    return fns[0](...args);
-  }
-  await fns[0](...args);
-  return graphqlFlow(...fns.slice(1))(...args);
-}
+const graphqlFlow = require('apollo-graphql-middleware');
 
 const wait = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
